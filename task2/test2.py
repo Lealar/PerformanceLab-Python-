f4fb8acebdf6815683592b5c0d07bae18f45ca87
Lealar <65033431+Lealar@users.noreply.github.com>
2020-11-16 17:37:31 +0300

    Update test2.py

--- task2/test2.py
+++ task2/test2.py
@@ -2,16 +2,20 @@ import sympy as sym
 
 lambd = sym.Symbol('lambd')
 sphere = [0, 0, 0]
-radius = 10.67
-line1 = [1, 0.5, 15]
-line2 = [43, -14.6, 0.04]
+radius = 10
+line1 = [20, 0, 60]
+line2 = [20, 0, -60]
 
 a = sym.solveset((line1[0] + (line2[0] - line1[0]) * lambd - sphere[0]) ** 2 + (
-            line1[1] + (line2[1] - line1[1]) * lambd - sphere[1]) ** 2 + (
-            line1[2] + (line2[2] - line1[2]) * lambd - sphere[2]) ** 2 - radius ** 2)
+        line1[1] + (line2[1] - line1[1]) * lambd - sphere[1]) ** 2 + (
+                         line1[2] + (line2[2] - line1[2]) * lambd - sphere[2]) ** 2 - radius ** 2)
 
 for i in a:
     PointX = (line1[0] + (line2[0] - line1[0]) * i)
     PointY = (line1[1] + (line2[1] - line1[1]) * i)
     PointZ = (line1[2] + (line2[2] - line1[2]) * i)
-    print(PointX, " ", PointY, " ", PointZ)
+    if "I" in (str(PointX) and str(PointY) and str(PointZ)):
+        print('Коллизий не найдено')
+        break
+    else:
+        print(PointX, " ", PointY, " ", PointZ)
